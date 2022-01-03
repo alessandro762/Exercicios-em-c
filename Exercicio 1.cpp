e7d9a1588f8fffc11619124a82b8b2b6668574f9
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-01-03 11:58:40 -0300

    Exercício 1
    Nesse exercício 1 foi feita a troca do valor de uma variável para outra.

---
+++ Exercicio 1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (){
+	
+	int a = 10;
+	
+	int b = 20;
+	
+	int c = 0;
+	
+	c=a
+	a=b
+	b=c
+	
+	
+	
+	
+	printf("O valor de  %d\n",a);
+	
+	printf("O valor de  %d\n",b);
+	
+}
