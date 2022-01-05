3dd6e06091a3fa4bac1cf84fd1bea11ee42bf64b
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-01-05 13:30:00 -0300

    Add files via upload

---
+++ exercicio2f.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+	
+	int x=1;
+	
+	int y=2;
+	
+	int z=y-x;
+	
+	printf ("O valor de z e de: %d \n",z);
+	
+	x=5;
+	
+	y=x+z;
+	
+	printf("O valor de x,y,z e : %d %d %d \n",x,y,z);
+	
+}
+	
