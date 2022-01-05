fa1924ef48fb1be189f2773df88b5cb2ad1d7bc4
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-01-05 12:46:44 -0300

    Add files via upload

---
+++ exercicio2d.c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (){
+	
+	int a=10;
+	
+	int b=a+1;
+	
+	a=b+1;
+	
+	b=a+1;
+	
+	printf(" O valor de a e: %d\n",a);
+	
+	a=b+1;
+	
+	printf("O valor de a,b e: %d %d",a,b);
+	
+}
