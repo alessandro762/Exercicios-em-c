7d99dd26ae87be1496003e6b07af058ae0d3c106
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-01-05 11:16:26 -0300

    Foi feita atribuições de valores a variáveis.

--- exercicio2.c
+++ exercicio2.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+
+int main (){
+	
+	int a=30;
+	int b=20;
+	int c= a+b;
+	
+	printf(" O valor de c e: %d\n",c);
+	
+	b=10;
+	
+	printf(" O valor de b,c e: %d %d\n",b,c);
+	
+	 c= a+b;
+	 
+	printf(" O valor de a,b,c e: %d %d %d\n", a,b,c);
+}
