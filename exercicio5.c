7c8ecda83c0debeb83656087eddd0a6913dac102
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-01-13 12:52:20 -0300

    Add files via upload
     Nesse exercício foi criado um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu antecessor.

---
+++ exercicio5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()  {
+
+	 
+	 //Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu antecessor.
+
+	int numero,antecessor;
+
+		printf("digite um numero:");
+
+		scanf ( numero);
+
+
+		antecessor= numero-1;
+
+		printf (" O antecessor de",numero"e:",antecessor);
+
+
+
+
+
+
+
+
+}
