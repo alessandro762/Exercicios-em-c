557cc09d73c5427746b0b2eb23df6563bf9b8b7f
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-01-14 07:54:42 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+//Escreva um algoritmo para ler um valor do teclado e escrever na tela o seu antecessor.
+
+int numero=0;
+
+  printf("digite um numero:");
+
+  scanf("%d",&numero);
+
+  
+  printf("O antecessor de:%d é numero:%d\n",numero,numero-1);
+
+}
