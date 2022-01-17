a961c3ee9d4e981670cabba1bc1efd9d0dc617b3
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-01-17 09:28:46 -0300

    Add files via upload
    Foi criado um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a
    área do retângulo.

---
+++ exercicio6(2).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+  //Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a
+//área do retângulo.
+
+int area;
+int base;
+int altura;
+
+printf("Digite uma base:");
+scanf ("%d",&base);
+printf ("Digite uma altura:");
+scanf ("%d",&altura);
+
+area=base*altura;
+
+printf("A area do retangulo e: %d",area);
+
+
+
+}
