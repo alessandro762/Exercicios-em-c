c06e1152f1b09d43ae57a78e0b146a728e1cba62
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-01-19 15:22:35 -0300

    exercicio 9
     Foi criado um algoritmo para ler o salário mensal atual de um funcionário e o percentual de reajuste.
    Calcular e escrever o valor do novo salário.

---
+++ exercicio9(1).c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+//Escreva um algoritmo para ler o salário mensal atual de um funcionário e o percentual de reajuste.
+//Calcular e escrever o valor do novo salário
+
+double salario_atual;
+double reajuste_salario;
+double novo_salario;
+
+printf("Digite o valor do seu salario:");
+
+scanf("%lf",&salario_atual);
+
+printf("Digite quantos porcento de reajuste:");
+
+scanf("%lf",&reajuste_salario);
+
+novo_salario=(salario_atual*reajuste_salario/100);
+
+novo_salario=salario_atual+novo_salario;
+
+printf("Seu novo salario e:%.2lf",novo_salario);
+
+
+}
