104e821efdda7e5c310ebba0b9938bced09b96a5
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-01-20 09:40:41 -0300

    exercicio 10
    Foi criado um algoritimo para ver o custo de um carro novo ao consumidor é a soma do custo de fábrica com a porcentagem do
    distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que o percentual do distribuidor
    seja de 28% e os impostos de 45%, escrever um algoritmo para ler o custo de fábrica de um carro,
    calcular e escrever o custo final ao consumidor.

---
+++ exercicio10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+//O custo de um carro novo ao consumidor é a soma do custo de fábrica com a porcentagem do
+//distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que o percentual do distribuidor
+//seja de 28% e os impostos de 45%, escrever um algoritmo para ler o custo de fábrica de um carro,
+//calcular e escrever o custo final ao consumidor.
+double custo_de_fabrica;
+double imposto;
+double percentual_distribuidor;
+double custo_para_consumidor;
+
+printf("O custo de fabrica e:");
+
+scanf("%lf",&custo_de_fabrica);
+
+percentual_distribuidor=(custo_de_fabrica*0.28)+custo_de_fabrica;
+
+imposto=(custo_de_fabrica*0.45)+custo_de_fabrica;
+
+custo_para_consumidor=percentual_distribuidor+imposto+custo_de_fabrica+custo_de_fabrica;
+
+printf("O valor do veiculo e:%.3lf",custo_para_consumidor);
+
+
+
+}
