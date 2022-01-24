16ca5ccea0919cc46a1dff10b2646e8131abdb76
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-01-24 15:14:48 -0300

    exercicio 15
    Foi criado um algoritimo  que Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo).

---
+++ exercicio15.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+  // Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo).
+  double numero;
+  printf("Digite um numero:");
+  scanf("%lf",&numero);
+  
+  if(numero > 0)
+  {
+    printf("O numero que voce digitou e negativo ");
+
+  }
+  else{
+  printf("O numero que voce digitou e positivo");
+
+  }
+  
+
+
+
+
+}
