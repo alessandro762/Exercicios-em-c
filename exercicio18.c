d30d717eb2a1190527d3bc54d344afbc7789959a
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-01-26 09:33:38 -0300

    exercicio 18
    Foi criado um algoritimo para Ler o ano atual e o ano de nascimento de uma pessoa. Escrever uma mensagem que diga se ela
    poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu).

---
+++ exercicio18.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+
+//Ler o ano atual e o ano de nascimento de uma pessoa. Escrever uma mensagem que diga se ela
+//poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu). 
+
+int ano1;
+int ano2=2022;
+printf("Digite o ano que voce nasceu:");
+scanf("%d",&ano1);
+
+ano2=ano2-ano1;
+
+if(ano2<16){
+printf("Voce ainda nao pode votar esse ano");
+}
+else{
+  printf("Voce ja pode votar esse ano");
+}
+
+
+
+
+
+
+
+
+}
