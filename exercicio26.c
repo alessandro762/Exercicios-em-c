9db5ea3bd2045adc496c9d7322634524001b45b4
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-02-02 10:09:31 -0300

    exercicio 26
    Foi criado um algoritimo que ler: quantidade atual em estoque, quantidade máxima em estoque e
    quantidade mínima em estoque de um produto. Calcular e escrever a quantidade média ((quantidade
    média = quantidade máxima + quantidade mínima)/2). Se a quantidade em estoque for maior ou igual
    a quantidade média escrever a mensagem 'Não efetuar compra', senão escrever a mensagem 'Efetuar
    compra.

---
+++ exercicio26.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+//Faça um algoritmo para ler: quantidade atual em estoque, quantidade máxima em estoque e
+//quantidade mínima em estoque de um produto. Calcular e escrever a quantidade média ((quantidade
+//média = quantidade máxima + quantidade mínima)/2). Se a quantidade em estoque for maior ou igual
+//a quantidade média escrever a mensagem 'Não efetuar compra', senão escrever a mensagem 'Efetuar
+//compra'.
+int maximo;
+int minimo;
+int atual;
+double media;
+
+printf("Qual a quantidade atual:\n");
+scanf("%d",&atual);
+
+printf("Qual a quantidade maxima:\n");
+scanf("%d",&maximo);
+
+printf("Qual a quantidade minima:\n");
+scanf("%d",&minimo);
+
+media=(maximo+minimo)/2;
+
+if (media>=maximo){
+  printf("Nao e necessario efetuar compra");
+}else{
+  printf("Necessario efetuar compra");
+}
+
+
+
+
+}
