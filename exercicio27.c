540c4d23d56726c21a3b47f55e8396bac0d9dc55
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-02-07 11:33:34 -0300

    exercicio 27
    Foi criado um algoritimo pra ler  um valor e escrever se é positivo, negativo ou zero.

---
+++ exercicio27.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (){
+
+//Ler um valor e escrever se é positivo, negativo ou zero.
+int numero;
+printf ("Digite um numero:\n");
+scanf("%d",&numero);
+
+if (numero<0){
+  printf ("Negativo");
+}else{
+  }if (numero>0){
+  printf("positivo");
+  }else{
+    }if (numero==0){
+      printf("Numero 0");
+    }
+  
+}
