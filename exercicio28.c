43ee15bb4af2dd57e8364303a76469955483cbbd
Alessandro <97038044+alessandro762@users.noreply.github.com>
2022-02-09 16:07:17 -0300

    exercicio 28
    Foi criado um algoritimo que l Ler 3 valores (considere que não serão informados valores iguais) e escrever o maior deles.

---
+++ exercicio28.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+//Ler 3 valores (considere que não serão informados valores iguais) e escrever o maior deles. 
+
+  int a=0;
+  int b=0;
+  int c=0;
+
+
+
+    printf("Digite um valor:\n");
+    scanf("%d",&a);
+    printf("Digite outro valor diferente do valor acima:\n");
+    scanf("%d",&b);
+    printf("Digite outro valor diferente dos valores acima:\n");
+    scanf("%d",&c);
+
+    if (a>b && a>c){
+      printf("O mair valor e:%d\n",a);
+    }else if(b>a && b>c){
+      printf("O maior valor e:%d\n",b);
+    }else
+      printf("O maior valor e:%d\n",c); 
+      
+    }
+
+  
+
+   
+  
